8783bde69588e88885f60344883afcd046764380
Kyle Ries <kyle.ries@gmail.com>
2012-12-11 19:44:47 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrackingMentorship::Application.config.secret_token = 'd9da80e8997e91b033592925a8426430f20128fc79933e23598a5325f201f2e025fb0beda92711b4f35d891fcb760ab0072a6dc94a2a5fda3353b5c3aae7c9e6'
